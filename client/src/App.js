7d57c61a244f0ef0dff3939d102681b20d1c46f0
vladislav babichev <babichev_vladislav@list.ru>
2021-03-29 20:51:46 +0300

    React router customization

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom'
+import AppRouter from './components/AppRouter'
 
 function App() {
   return (
     <BrowserRouter>
-      WORKING
+      <AppRouter/>
     </BrowserRouter>
   );
 }
--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
 import {Switch, Route, Redirect} from 'react-router-dom'
-
+import { authRoutes } from '../routes';
+import { publicRoutes } from '../routes'
+import { SHOP_ROUTE } from '../utils/consts';
 const DevicePage = () => {
+    const isAuth = false;
+
     return(
-        <div>
-            AppRouter
-        </div>
+        <Switch>
+            {isAuth === true && authRoutes.map(({path, Component}) => 
+                <Route key={path} path={path} component={Component} exact/>
+            )}
+            {publicRoutes.map(({path, Component}) => 
+                <Route key={path} path={path} component={Component} exact/>
+            )}
+            <Redirect to={SHOP_ROUTE}/>
+        </Switch>
     )
 }
 
